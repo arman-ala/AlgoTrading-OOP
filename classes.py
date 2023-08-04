c7857b1300e849191f812a820f45ab4c49b22eb9
Arman Ala <arman.ala.developer@gmail.com>
2023-08-05 01:24:46 +0330

    classes.py -> Genre class is completed for now.

--- classes.py
+++ classes.py
@@ -3,7 +3,17 @@
 # Private Library Management Classes
 
 class Genre:
-    pass
+    def __init__(self, *args):
+        self.genres = []
+        if len(args) > 0:
+            self.genres.extend(args)
+
+    def add_genre(self, genre):
+        if genre:
+            self.genres.append(genre)
+            return True
+        else:
+            return False
 
 
 class Author:
