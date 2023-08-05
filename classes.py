25d31125083192bdb1d279477826c86d62c88340
Arman Ala <arman.ala.developer@gmail.com>
2023-08-05 09:13:47 +0330

    classes.py -> Book, Author and Genre classes are linked to each other and all 3 classes have some kind of error handling.

--- classes.py
+++ classes.py
@@ -6,10 +6,23 @@ authors = []
 books = []
 
 class Genre:
-    def __init__(self, genre_name):
+    def __init__(self, genre_name=None):
         self.genre = None
         if genre_name is not None:
-            self.genre = self.genre_name
+            self.genre = genre_name
+        else:
+            self.genre = input("Please enter a Genre: ")
+            # checking existance of genre in the list
+            exists = False
+            for genre in genres:
+                if genre_name == genre.genre:
+                    exists = True
+            if exists == False:
+                genres.append(self)
+    
+    
+    def __str__(self):
+        return self.genre
 
 
 class Author:
@@ -17,32 +30,51 @@ class Author:
         self.name = None
         self.age = None
         self.email = None
-        if author_name is not None:
+        if author_name is not None and author_name != '':
             self.name = author_name
-        if author_age is not None:
+        else:
+            self.name = input("Please enter author\'s name: ")
+        if author_age is not None and author_age != '':
             self.age = author_age
-        if author_email is not None:
+        else:
+            self.age = int(input("Please enter author\'s age: "))
+        if author_email is not None and author_email != '':
             self.email = author_email
-
-
+        else:
+            self.email = input("Please enter author\'s email: ")
+        # checking existance of author in the list
+        exists = False
+        for author in authors:
+            if author_name == author.name:
+                exists = True
+        if exists == False:
+            authors.append(self)
+    
+    
     def __str__(self):
         return f"{self.name} - {self.age} - {self.email}"
 
 
 class Book:
-    def __init__(self, book_title, author_name, book_genre, book_pushed_date):
+    def __init__(self, book_title, book_pushed_date,\
+        book_genre=None, author_name=None, author_age=None, author_email=None):
         self.title = None
         self.author = None
         self.genre = None
         self.published_date = None
-        if book_title is not None:
+        if book_title is not None or book_title != '':
             self.title = book_title
-        if book_genre is not None:
-            self.genre = book_genre
-        if book_pushed_date is not None:
+        if book_pushed_date is not None or book_pushed_date != '':
             self.published_date = book_pushed_date
-        if author_name is not None:
-            self.author = author_name
+        self.genre = Genre(book_genre)
+        self.author = Author(author_name, author_age, author_email)
+        # checking existance of author in the list
+        exists = False
+        for book in books:
+            if book_title == book.title:
+                exists = True
+        if exists == False:
+            books.append(self)
 
 
 def add_genre(genre):
