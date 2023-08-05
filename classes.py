0582ac9b49eab50524f22fd0d93e3cc9208eb63b
Arman Ala <arman.ala.developer@gmail.com>
2023-08-05 04:01:24 +0330

    classes.py -> Book class is completed and add_author is working and add_book is also implemented.

--- classes.py
+++ classes.py
@@ -3,14 +3,13 @@
 # Private Library Management Classes
 genres = []
 authors = []
-
+books = []
 
 class Genre:
     def __init__(self, genre_name):
         self.genre = None
         if genre_name is not None:
             self.genre = self.genre_name
-
 
 
 class Author:
@@ -30,15 +29,32 @@ class Author:
         return f"{self.name} - {self.age} - {self.email}"
 
 
+class Book:
+    def __init__(self, book_title, author_name, book_genre, book_pushed_date):
+        self.title = None
+        self.author = None
+        self.genre = None
+        self.published_date = None
+        if book_title is not None:
+            self.title = book_title
+        if book_genre is not None:
+            self.genre = book_genre
+        if book_pushed_date is not None:
+            self.published_date = book_pushed_date
+        if author_name is not None:
+            self.author = author_name
+
+
 def add_genre(genre):
     if genre is not None:
         genres.append(genre)
 
 
-def add_author(self, author_name):
+def add_author(author_name):
     if author_name is not None:
         authors.append(author_name)
 
 
-class Book:
-    pass
+def add_book(book):
+    if book is not None:
+        books.append(book)
